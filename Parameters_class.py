7ed648e49e7474b5c87d9755f01a30cc6a1fb40e
Olga <okavvada@gmail.com>
2017-02-22 16:53:30 -0800

    read from dataframe

--- Parameters_class.py
+++ Parameters_class.py
@@ -1,19 +1,22 @@
 from __future__ import division
 import numpy as np
 
+from layers import Layers_values
+
 
 class Parameters_values():
     def __init__(self):
-        self.weight_1 = 0.5 #percent of pop served
-        self.weight_2 = 0.5
-        self.weight_3 = 0.5
-        self.weight_4 = 0.5
-        self.weight_5 = 0.5
-        self.weight_6 = 0.5
-        self.weight_7 = 0.5
-        self.weight_8 = 0.5
-        self.weight_9 = 0.5
-        self.weight_10 = 0.5
+        self.weight_1 = 0.109
+        self.weight_2 = 0.038
+        self.weight_3 = 0.021
+        self.weight_4 = 0.168
+        self.weight_5 = 0.109
+        self.weight_6 = 0.053
+        self.weight_7 = 0.259
+        self.weight_8 = 0.259
+        self.weight_9 = 0.259
+        self.weight_10 = 0.259
+        self.weight_11 = 0.259
 
     def max_min(self, percent):
         maxmin = {
@@ -27,6 +30,7 @@ class Parameters_values():
         'weight_8_min': (1-percent)*self.weight_8,
         'weight_9_min': (1-percent)*self.weight_9,
         'weight_10_min': (1-percent)*self.weight_10,
+        'weight_11_min': (1-percent)*self.weight_11,
         'weight_1_max': (1+percent)*self.weight_1,
         'weight_2_max': (1+percent)*self.weight_2,
         'weight_3_max': (1+percent)*self.weight_3,
@@ -36,7 +40,8 @@ class Parameters_values():
         'weight_7_max': (1+percent)*self.weight_7,
         'weight_8_max': (1+percent)*self.weight_8,
         'weight_9_max': (1+percent)*self.weight_9,
-        'weight_10_max': (1+percent)*self.weight_10}
+        'weight_10_max': (1+percent)*self.weight_10,
+        'weight_11_max': (1+percent)*self.weight_11}
         
         return maxmin
 
@@ -53,6 +58,7 @@ class Parameters_values():
         new_params.weight_8 = np.random.uniform(maxmin['weight_8_min'],maxmin['weight_8_max'])
         new_params.weight_9 = np.random.uniform(maxmin['weight_9_min'],maxmin['weight_9_max'])
         new_params.weight_10 = np.random.uniform(maxmin['weight_10_min'],maxmin['weight_10_max'])
+        new_params.weight_11 = np.random.uniform(maxmin['weight_11_min'],maxmin['weight_11_max'])
         
         return new_params
 
@@ -109,12 +115,18 @@ class Parameters_values():
                 new_params.weight_10 = self.weight_10 - (maxmin['weight_10_max'] - maxmin['weight_10_min'])/10 
             if direction == 'plus':
                 new_params.weight_10 = self.weight_10 + (maxmin['weight_10_max'] - maxmin['weight_10_min'])/10 
+        if parameter == 'weight_11':  
+            if direction == 'minus':  
+                new_params.weight_11 = self.weight_11 - (maxmin['weight_11_max'] - maxmin['weight_11_min'])/10 
+            if direction == 'plus':
+                new_params.weight_11 = self.weight_11 + (maxmin['weight_11_max'] - maxmin['weight_11_min'])/10 
         
         return new_params
 
 
-def run_simulation(layers, percent, analysis = 'Normal'):
+def run_simulation(dataframe, percent, analysis = 'Uncertainty'):
     Parameters = Parameters_values()
+    Layers = Layers_values(dataframe)
     if analysis == 'Uncertainty':
             perturb_Parameters = Parameters.uncertainty(percent)
             Parameters = perturb_Parameters
@@ -122,8 +134,10 @@ def run_simulation(layers, percent, analysis = 'Normal'):
             perturb_Parameters = Parameters.sensitivity(parameter, direction)
             Parameters = perturb_Parameters
 
-    output = (layers.layer_1*Parameters.weight_1 + layers.layer_2*Parameters.weight_2 + layers.layer_3*Parameters.weight_3 + layers.layer_4*Parameters.weight_4 + layers.layer_5*Parameters.weight_5 + layers.layer_6*Parameters.weight_6 +
-        layers.layer_7*Parameters.weight_7 + layers.layer_8*Parameters.weight_8 + layers.layer_9*Parameters.weight_9 + layers.layer_10*Parameters.weight_10)
+    output = (Layers.layer_1*Parameters.weight_1 + Layers.layer_2*Parameters.weight_2 + Layers.layer_3*Parameters.weight_3 + Layers.layer_4*Parameters.weight_4 + Layers.layer_5*Parameters.weight_5 + Layers.layer_6*Parameters.weight_6 +
+        Layers.layer_7*Parameters.weight_7 + Layers.layer_8*Parameters.weight_8 + Layers.layer_9*Parameters.weight_9 + Layers.layer_10*Parameters.weight_10 + Layers.layer_11*Parameters.weight_11)
+
+    point_name = Layers.name
 
     return output
 
--- layers.py
+++ layers.py
@@ -1,10 +1,41 @@
-layer_1 = 1
-layer_2 = 1
-layer_3 = 1
-layer_4 = 1
-layer_5 = 1
-layer_6 = 1
-layer_7 = 1
-layer_8 = 1
-layer_9 = 1
-layer_10 = 1
+values = {1:'VALUE_',
+			2: 'VALUE_1',
+			3: 'VALUE_12',
+			4: 'VALUE_1_13',
+			5: 'VALUE_1_14',
+			6: 'VALUE_1_15',
+			7: 'VALUE_1_16',
+			8: 'VALUE_1_17',
+			9: 'VALUE_1_18',
+			10: 'VALUE_1_19',
+			11: 'VALUE_1_20'}
+
+class Layers_values():
+	def __init__(self, dataframe):
+		self.dataframe = dataframe
+		self.layer_1 = self.get_value_layers(1)
+		self.layer_2 = self.get_value_layers(2)
+		self.layer_3 = self.get_value_layers(3)
+		self.layer_4 = self.get_value_layers(4)
+		self.layer_5 = self.get_value_layers(5)
+		self.layer_6 = self.get_value_layers(6)
+		self.layer_7 = self.get_value_layers(7)
+		self.layer_8 = self.get_value_layers(8)
+		self.layer_9 = self.get_value_layers(9)
+		self.layer_10 = self.get_value_layers(10)
+		self.layer_11 = self.get_value_layers(11)
+		self.name = self.get_name_pont()
+
+	def get_value_layers(self, layer_name):
+		layer_value = self.dataframe.iloc[0][values[layer_name]]
+		return layer_value
+
+	def get_name_pont(self):
+		layer_value = self.dataframe.iloc[0]['id_val']
+		return layer_value
+
+
+
+
+
+
